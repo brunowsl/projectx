30bb2b95c6318a9f383fe5dda52fc377547994c8
Bruno Wagner de Sousa Lobão <brunowsl123@gmail.com>
2021-11-06 01:14:11 -0300

    ok

---
+++ components/main/TransactionsList.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-row> </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
--- pages/main/index.vue
+++ pages/main/index.vue
@@ -1,15 +1,66 @@
 <template>
-    <v-row justify="center" align="center">
-        <v-col>
-            
-        </v-col>
-    </v-row>
+  <v-row>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar color="primary" :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title class="title-text" v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <v-card> </v-card>
+      </v-container>
+    </v-main>
+  </v-row>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/main',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'PROJETOX',
+    }
   },
 }
 </script>
